0087f64b05d9cfbfc914c6d509c7ac90fd7e537c
VincentCSS <vincenthtml@gmail.com>
2020-04-21 16:53:45 -0600

    updated dummy data accordingly

--- db/dummyData.js
+++ db/dummyData.js
@@ -6,26 +6,17 @@ const bugExample = {
     profilePicture: 'FILL_ME_IN',
   },
   description: 'FILL_ME_IN',
+  dueDate: 'Date.now() + 6.04e+8 (1 week from now) || Date.now() + (6.04e+8 * 2) (2 weeks from now)',
   tags: ['FILL_ME_IN'],
+  threat: 'Critical || Important || Low-Priority',
 };
 
 const postExample = {
-  bug: 'Must be unique and reference bug ticket',
-  title: 'Same as bug ticket',
   author: {
-    name: 'Same as bug ticket',
-    profilePicture: 'Same as bug ticket',
+    name: 'FILL_ME_IN',
+    profilePicture: 'FILL_ME_IN',
   },
-  description: 'Same as bug ticket',
-  posts: [{
-    author: {
-      name: 'FILL_ME_IN',
-      profilePicture: 'FILL_ME_IN',
-    },
-    text: 'FILL_ME_IN',
-    images: ['FILL_ME_IN MAX OF 3'],
-    createdAt: 'Whatever Date.now() spits backs',
-  },
-  // however many more you feel like.
-  ],
+  text: 'FILL_ME_IN',
+  images: ['FILL_ME_IN MAX OF 3'],
+  createdAt: 'Whatever Date.now() spits backs',
 };
